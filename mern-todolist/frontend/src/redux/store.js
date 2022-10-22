5a0f1ff82b48006af2dc9560e3f025c9446fb502
HOSENUR <nur08439@gmail.com>
2022-10-22 18:12:51 +0530

    Initialised Redux ToolKIt

--- mern-todolist/frontend/src/redux/store.js
+++ mern-todolist/frontend/src/redux/store.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import {  applyMiddleware } from "redux";
+import {configureStore} from '@reduxjs/toolkit'
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+const store = configureStore({
+  reducer: rootReducer,
+},
+composeWithDevTools(applyMiddleware(thunk))
 );
-
+  // rootReducer,
 export default store;
