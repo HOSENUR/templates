6c94d9e8875e662e88bb0855040c7522ae8498c5
HOSENUR <nur08439@gmail.com>
2022-10-23 01:35:30 +0530

    Rearranged Files

--- react-redux-toolkit/src/redux/features/counterSlice.js
+++ react-redux-toolkit/src/redux/features/counterSlice.js
@@ -12,6 +12,12 @@ export const counterSlice = createSlice({
         },
         decrement: state => {
             state.value -= 1
+        },
+        reset: state => {
+            state.value = 0
+        },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
         }
     }
 })
